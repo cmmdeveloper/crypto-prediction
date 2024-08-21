d78c055576befa2ed0317ae268bf2a2d0fefdf11
cmmdeveloper <carena.manzano.dev@gmail.com>
2024-08-21 16:16:31 -0400

    Add files via upload

---
+++ crypto-prediction/src/model.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+def train_model(X_train, y_train):
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    return mse, y_pred
